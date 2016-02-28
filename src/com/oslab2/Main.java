f3e7c86a280bc8dbf0510dd6599d5e9054a4bd95
Evan Feng <evanfeng55@gmail.com>
2016-02-27 20:18:10 -0500

    implemented random function

--- src/com/oslab2/Main.java
+++ src/com/oslab2/Main.java
@@ -2,13 +2,16 @@ package com.oslab2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 
 public class Main {
 
     private static final String INPUT_FILE = "input.txt";
+    private static final String RANDOM_NUMBERS_FILE = "random-numbers.txt";
 
     private static int number_of_processes;
     private static ArrayList<Process> processes = null;
@@ -19,8 +22,11 @@ public class Main {
         File file = new File(INPUT_FILE);
         processes = readInputFromFile(file);
 
-        printProcessList(processes);
+//        printProcessList(processes);
+
+        randomOS(1);
     }
+
 
 
     private static ArrayList readInputFromFile(File file) throws FileNotFoundException {
@@ -44,10 +50,61 @@ public class Main {
     }
 
 
+
+
+
+    // generate a random number
+    private static int randomOS(int u) throws FileNotFoundException {
+
+        ArrayList<Integer> randomintegers = new ArrayList<Integer>();
+
+        File file = new File(RANDOM_NUMBERS_FILE);
+        Scanner input =  new Scanner(file);
+
+        while(input.hasNextLine()){
+            try{
+                randomintegers.add(Integer.parseInt(input.next()));
+            }catch(Exception e){
+            }
+        }
+
+
+        //get a random element from the arraylist of integers
+        Random randomGen = new Random();
+        int randomint = randomintegers.get(randomGen.nextInt(randomintegers.size()));
+        System.out.println("RANDOM INT: " + randomint);
+
+        return 1 + (randomint % u);
+    }
+
+
+
+    // print the list of all processes
     private static void printProcessList(ArrayList<Process> processes){
         for(Process process : processes){
             System.out.println(process.toString());
         }
     }
 
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
